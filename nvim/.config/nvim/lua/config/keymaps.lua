ced3686f78cc9a4d6a29c7f2e08429f0a6fa3c55
Benjamin Griffiths <drjamin1990@gmail.com>
2025-09-24 10:28:08 +0100

    added in a keymap to rename variables globally

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -4,6 +4,7 @@ function M.setup()
   vim.keymap.set("n", "<leader>q", vim.diagnostic.open_float)
   vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
   vim.keymap.set("n", "<Esc><Esc>", "<cmd>nohlsearch<CR>", { silent = true })
+  vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "LSP: Rename symbol" })
 end
 
 return M
