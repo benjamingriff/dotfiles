ea0a4096cb427bf6d88b5ee368f3664056ecfd20
Benjamin Griffiths <drjamin1990@gmail.com>
2025-10-01 15:45:16 +0100

    updating nvim setup

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -5,6 +5,7 @@ function M.setup()
   vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
   vim.keymap.set("n", "<Esc><Esc>", "<cmd>nohlsearch<CR>", { silent = true })
   vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "LSP: Rename symbol" })
-end
+  vim.keymap.set("n", "<leader>nl", [[:s/\%#/\r/<CR>]], { desc = "Add new line at cursor", silent = true }
+)end
 
 return M
--- nvim/.config/nvim/lua/plugins/prettier.lua
+++ nvim/.config/nvim/lua/plugins/prettier.lua
@@ -1,24 +1,23 @@
 return {
   {
     "stevearc/conform.nvim",
-    event = "BufWritePre",
-    config = function()
-      require("conform").setup({
-        formatters_by_ft = {
-          typescript = { "prettier" },
-          javascript = { "prettier" },
-          json = { "prettier" },
-          yaml = { "prettier" },
-        },
-      })
-
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        pattern = { "*.ts", "*.js", "*.json", "*.yaml" },
-        callback = function(args)
-          require("conform").format({ bufnr = args.buf })
-        end,
-      })
-    end,
-  }
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      formatters_by_ft = {
+        typescript = { "prettier" },
+        javascript = { "prettier" },
+        json = { "prettier" },
+        yaml = { "prettier" },
+      },
+      format_on_save = function(bufnr)
+        local name = vim.api.nvim_buf_get_name(bufnr)
+        if name:match("%.ts$") or name:match("%.js$") or name:match("%.json$")
+          or name:match("%.ya?ml$")
+        then
+          return { lsp_fallback = false, timeout_ms = 2000 }
+        end
+        return nil
+      end,
+    },
+  },
 }
-
