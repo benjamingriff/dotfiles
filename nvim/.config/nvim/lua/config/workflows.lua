3872912e8a97720fe3b8972c391f76811e51423c
Benjamin Griffiths <drjamin1990@gmail.com>
2025-11-04 12:59:46 +0000

    adding in both vault directories and shortcuts

--- nvim/.config/nvim/lua/config/workflows.lua
+++ nvim/.config/nvim/lua/config/workflows.lua
@@ -1,16 +1,27 @@
 local M = {}
 
 function M.setup()
-  vim.keymap.set("n", "<leader>oo", "<cmd>cd ~/repos/vault<cr>")
+  vim.keymap.set("n", "<leader>opo", "<cmd>cd ~/repos/vault<cr>")
   vim.keymap.set(
     "n",
-    "<leader>of",
+    "<leader>opf",
     "<cmd>Telescope find_files search_dirs={'/Users/benjamingriffiths/repos/vault'}<cr>"
   )
   vim.keymap.set(
     "n",
-    "<leader>og",
+    "<leader>opg",
     "<cmd>Telescope live_grep search_dirs={'/Users/benjamingriffiths/repos/vault'}<cr>"
+  )
+  vim.keymap.set("n", "<leader>oo", "<cmd>cd ~/repos/open-vault<cr>")
+  vim.keymap.set(
+    "n",
+    "<leader>of",
+    "<cmd>Telescope find_files search_dirs={'/Users/benjamingriffiths/repos/open-vault'}<cr>"
+  )
+  vim.keymap.set(
+    "n",
+    "<leader>og",
+    "<cmd>Telescope live_grep search_dirs={'/Users/benjamingriffiths/repos/open-vault'}<cr>"
   )
 end
 
