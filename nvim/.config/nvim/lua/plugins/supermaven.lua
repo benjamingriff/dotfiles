25d8dfd631813f39bf9078cb28ef190af16ee0da
Benjamin Griffiths <drjamin1990@gmail.com>
2026-04-02 11:05:44 +0100

    removing ai autocomplete

--- nvim/.config/nvim/lua/plugins/supermaven.lua
+++ nvim/.config/nvim/lua/plugins/supermaven.lua
@@ -1,19 +1,19 @@
 return {
-  {
-    "supermaven-inc/supermaven-nvim",
-    config = function()
-      require("supermaven-nvim").setup({
-        keymaps = {
-          accept_suggestion = "<C-y>",
-          clear_suggestion = "<C-n>",
-          accept_word = "<C-w>",
-        },
-        ignore_filetypes = { cpp = true }, -- or { "cpp", }
-        color = {
-          suggestion_color = "#ccff00",
-          cterm = 93,
-        },
-      })
-    end
-  }
+  -- {
+  --   "supermaven-inc/supermaven-nvim",
+  --   config = function()
+  --     require("supermaven-nvim").setup({
+  --       keymaps = {
+  --         accept_suggestion = "<C-y>",
+  --         clear_suggestion = "<C-n>",
+  --         accept_word = "<C-w>",
+  --       },
+  --       ignore_filetypes = { cpp = true }, -- or { "cpp", }
+  --       color = {
+  --         suggestion_color = "#ccff00",
+  --         cterm = 93,
+  --       },
+  --     })
+  --   end
+  -- }
 }
